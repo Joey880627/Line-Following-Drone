367df207527c52d9f734d47ba588cbabb9a3be03
Joey880627 <47892850+Joey880627@users.noreply.github.com>
2019-07-14 05:26:02 +0800

    Add files via upload

--- src/color_recognization.py
+++ src/color_recognization.py
@@ -1,3 +1,43 @@
 import cv2
-image = cv2.imread('../data/red_test.jpg')
-cv2.imshow('Image', image)
+import numpy as np
+image_path = '../data/red_test.jpg'
+def image_color(image_path):
+    colors = ['blue', 'green', 'red', None]
+    color_index = -1
+    image = cv2.imread(image_path)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    lower_blue=np.array([80,80,80])
+    upper_blue=np.array([100,255,255])
+    lower_green=np.array([35,43,46])
+    upper_green=np.array([55,255,255])
+    lower_red_1=np.array([170,100,100])
+    upper_red_1=np.array([180,255,255])
+    lower_red_2=np.array([0,100,100])
+    upper_red_2=np.array([8,255,255])
+
+    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+    mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
+    mask_red_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
+    mask_red = cv2.bitwise_or(mask_red_1, mask_red_2)
+
+    color_sum = [mask_blue.sum(), mask_green.sum(), mask_red.sum()]
+
+    '''
+    res_blue = cv2.bitwise_and(image, image, mask = mask_blue)
+    res_green = cv2.bitwise_and(image, image, mask = mask_green)
+    res_red = cv2.bitwise_and(image, image, mask = mask_red)
+    cv2.imshow('original image', image)
+    cv2.imshow('res_blue', res_blue)
+    cv2.imshow('res_green', res_green)
+    cv2.imshow('res_red', res_red)
+    cv2.waitKey()
+    cv2.destroyAllWindows()'''
+    
+    max_color_index = np.argmax(color_sum)
+    if color_sum[max_color_index] >= 2000000:
+        color_index = max_color_index
+    return colors[color_index]
+if __name__ == '__main__':
+    print(image_color(image_path))
